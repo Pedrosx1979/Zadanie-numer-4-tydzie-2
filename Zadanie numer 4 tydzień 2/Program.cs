81ac5e0c73643383aaa587f7f8486c6587979c3f
Piotr Szydlak <pedrosx@o2.pl>
2023-11-30 11:17:57 +0100

    Add project files.

---
+++ Zadanie numer 4 tydzień 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_numer_4_tydzień_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            string label = "Szkoła DotNeta";
+            double b = 12.5;
+            Console.WriteLine(a);
+            Console.WriteLine(label);
+            Console.WriteLine(b);
+
+
+            
+            
+        }
+    }
+}
